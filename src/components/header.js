dcf081dcbea28bf52cc6ea3eb05be02358dd9708
ehab-emad <ehabemad515@gmail.com>
2024-09-22 22:25:02 +0300

    first1 commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 import './Header.css'; // استيراد ملف الـ CSS
-import Sidenav from './sidenav';
-import BottomBar from './bottombar';
-import m1 from '../asssets/grid-3x3-gap-fill.svg'
-import m2 from '../asssets/Website_Ico.png'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './sidenav';
-import Sidebar from './bottombar';
+
 import './Header.css'
 
 function Header() {
